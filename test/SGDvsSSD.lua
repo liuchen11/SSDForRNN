49e6b5b498fa0f907b68acd4dc041bc385e70f31
liuchen11 <liuchen112013@gmail.com>
2016-04-23 17:03:47 +0200

    fix a bug

--- test/SGDvsSSD.lua
+++ test/SGDvsSSD.lua
@@ -5,23 +5,23 @@ require 'ssd.ssd'
 
 rnn1={i2h=nil,h2h=nil,h2o=nil,buffer=0,__init__=RNN.__init__,run1Token=RNN.run1Token,runTokens=RNN.runTokens,update=RNN.update}
 rnn2={i2h=nil,h2h=nil,h2o=nil,buffer=0,__init__=RNN.__init__,run1Token=RNN.run1Token,runTokens=RNN.runTokens,update=RNN.update}
-N=100
-H=100
+N=10
+H=10
 K=10
 S=10
 iters=10
 
 rnn1:__init__(N,H,K)
 rnn2:__init__(N,H,K)
-rnn2.i2h.weight:copy(rnn2.i2h.weight)
-rnn2.h2h.weight:copy(rnn2.h2h.weight)
-rnn2.h2o.weight:copy(rnn2.h2o.weight)
+rnn2.i2h.weight:copy(rnn1.i2h.weight)
+rnn2.h2h.weight:copy(rnn1.h2h.weight)
+rnn2.h2o.weight:copy(rnn1.h2o.weight)
 -- print('input',rnn.i2h.weight)
 -- print('recur',rnn.h2h.weight)
 -- print('output',rnn.h2o.weight)
 
 initial=torch.randn(H)
-states=torch.randn(S,N)
+states=torch.ones(S,N)
 ground_truth=torch.zeros(S,K)
 for i=1,S do
 	spot=math.floor(math.random()*K)+1
@@ -32,7 +32,7 @@ time=0
 for i=1,iters do
 	local begin=os.clock()
 	local err=sgd(rnn1,initial,states,ground_truth,S)
-	rnn1:update(0.01)
+	rnn1:update(0.2)
 	local finish=os.clock()
 	time=time+finish-begin
 	print(i,err)
@@ -43,7 +43,7 @@ time=0
 for i=1,iters do
 	local begin=os.clock()
 	local err=ssd(rnn2,initial,states,ground_truth,S)
-	rnn2:update(1)
+	rnn2:update(0.5)
 	local finish=os.clock()
 	time=time+finish-begin
 	print(i,err)
