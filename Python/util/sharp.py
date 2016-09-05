4d35fdb72fbeb8ee6e7c84cdffd5a26630c4a92e
liuchen11 <liuchen112013@gmail.com>
2016-09-05 10:50:59 -0400

    fix sharp op bug

--- Python/util/sharp.py
+++ Python/util/sharp.py
@@ -4,7 +4,8 @@ import randSVD
 import numpy as np
 
 def sharp(input):
-    U,s,V=np.linalg.svd(input,full_matrices=1)
-    U=U[:,0:V.shape[1]]
-    V=V[:,0:U.shape[1]].transpose()
+    U,s,V=np.linalg.svd(input,full_matrices=False)
+    # U=U[:,0:V.shape[1]]
+    # V=V[0:U.shape[0],:]
     return np.dot(U,V)*np.sum(s)
+
